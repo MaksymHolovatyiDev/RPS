16e220597b910ad0240dc8222f38a818331a524d
MaksymHolovatyi <maksym.holovatyi.dev@gmail.com>
2023-08-28 11:24:14 +0300

    Add correct actions

--- __tests__/MainScreen.test.tsx
+++ __tests__/MainScreen.test.tsx
@@ -8,7 +8,7 @@ describe('Main render', () => {
     jest.useFakeTimers();
     const tree = render(<Main />).toJSON();
 
-    expect(screen.getByText('Scores')).toBeDefined();
+    expect(screen.getByText('Score')).toBeDefined();
     expect(tree).toMatchSnapshot();
   });
 });
